be82c2aead99409ed8ba47cf7035656530169a81
Pascal Pfiffner <phase.of.matter@gmail.com>
2012-12-18 12:32:04 -0500

    Initial Commit

---
+++ growth-charts-helper/CHDocument.h
@@ -0,0 +1,13 @@
+//
+//  CHDocument.h
+//  growth-charts-helper
+//
+//  Created by Pascal Pfiffner on 12/18/12.
+//  Copyright (c) 2012 CHIP. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CHDocument : NSDocument
+
+@end
---
+++ growth-charts-helper/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  growth-charts-helper
+//
+//  Created by Pascal Pfiffner on 12/18/12.
+//  Copyright (c) 2012 CHIP. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
